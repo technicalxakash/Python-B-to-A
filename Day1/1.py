7d294cdd3c904a51fb626d8b10db74e37b3834e0
technicalxakash <akashr2468@gmail.com>
2026-01-26 18:46:44 +0530

    day1

---
+++ Day1/1.py
@@ -0,0 +1,27 @@
+a=10
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+#swap two numbers
+#method1
+a=10
+b=20
+
+a,b=b,a
+print(a,b)
+
+#method2
+x=20
+y=30
+
+x=x+y
+y=x-y
+x=x-y
+print(x,y)
