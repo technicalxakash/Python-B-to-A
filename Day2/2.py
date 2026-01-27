f03a9d83b741583e75d9478c178f84afe366a885
technicalxakash <akashr2468@gmail.com>
2026-01-27 12:25:06 +0530

    day2

---
+++ Day2/2.py
@@ -0,0 +1,55 @@
+#input parameters
+"""
+a=int(input("boy age : "))
+b=int(input("girl age : "))
+age = a-b
+
+print(f"there age differnce is this {age}.")#this is F String
+
+
+#logical operator
+
+a=int(input("enter a number1 =  "))
+b=int(input("enter a number2 =  "))
+
+if(a>10 and b>10):
+    print("both number gretaer tahn 10")
+
+else:
+    print("both number not  gretaer tahn 10")
+
+if(a<5 or b<5):
+    print(" yes one number  is less than 5")
+else:
+    print(" yes one number  is not  less than 5")
+
+if(a < b):
+    print("yes the first number is not greater than the second")
+else:
+    print("greter than the second")
+
+   
+
+#comparison operator
+age=int(input("enter you age :   "))
+
+if(age<=18):
+    print("you are an adult")
+else:
+    print("you are an minor")
+
+ """
+
+#membership operators
+
+a=input("enter a string : ")
+
+if("a" in a):
+    print("yes a in a ")
+else:
+    print("not")
+
+if("python" not in a):
+    print("yes")
+else:
+    print("yes")
