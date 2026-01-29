cf91d76589258b8bb309f7390abb5a55679501f2
technicalxakash <akashr2468@gmail.com>
2026-01-29 23:33:18 +0530

    day4

--- Day4/4.py
+++ Day4/4.py
@@ -28,3 +28,73 @@
 #     i-=1
 # print("Happy new year")
 
+
+
+# words=input("enter the sentence : ")
+
+# results=""
+
+# for ch in words:
+#     if ch>="A" and ch<="Z":
+#         results=results+ch.lower()
+#     else:
+#         results=results+ch.upper()
+# print(results)
+
+
+
+#for loop
+# for i in range(1,11):
+#     print(i)
+
+# bag=["red", "greeen", "blue"]
+
+# for baga in bag:
+#     print(baga,end=" ") 
+
+# name="akash"
+
+# for index,i in enumerate(name):
+#     print(f"{i} du index {index}")
+
+
+# a={"A":"akash","B":"bag","C":"cat"}
+# print(type(a))
+# print(a.items())
+
+# for i,j in a.items():
+#     print(i,j)
+
+# for i in range(1,6):
+#     for j in range(1,11):
+#         print(f"{i}X{j}==={i*j}", end=",")
+#     print()
+
+# for i in range(1,31):
+#     if(i%3==0):
+#         print(i)
+#     else:
+#         continue
+
+
+# res=0
+
+# for i in range(1,11):
+#     res=res+i
+# print(res)
+    
+
+
+# char="akash"
+# for i in char:
+#     print(i , end=" ")
+    
+
+vow="this is my name"
+count=0
+for i in vow:
+    if i=="a" or i=="e" or i=="o" or i=="u":
+          count=count+1
+    else:
+         continue
+print(count) 
