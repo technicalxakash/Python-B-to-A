e07188ac64f1e1aed027c33b2e38d5630e1c1cab
technicalxakash <akashr2468@gmail.com>
2026-02-03 13:03:26 +0530

    new

---
+++ Day9/9.py
@@ -0,0 +1,57 @@
+# for i in range(1,6):
+#     print(i)
+
+
+# add=0
+# for i in range(1,6):
+#     add=add+i
+# print(add)
+
+
+# for i  in range(0,11,2):
+#     if i==0:
+#         continue
+#     print(i)
+
+
+
+# for i in range(1,11):
+#     print(f"3X{i}={i*3}")
+
+
+#calculate factorial of n
+# num=5
+# for i in range(1,num+1):
+#     fact=i*i
+# print(fact)
+
+# num=list(map(int,input("enter the numbers").split(",")))
+
+# for i in num:
+#     print(i*i,end=" ")
+
+# num=list(map(int,input("enter the numbers").split(",")))
+
+# maxi=num[0]
+# mini=num[0]
+
+# for i in num:
+#     if i>maxi:
+#         maxi=i
+#     if i<mini:
+#         mini=i
+# print(mini)
+# print(maxi)
+
+
+# inu=[10,20,30,40,10,20,30,50]
+# inu1=[]
+
+# for i in inu:
+#     if i in inu1:
+#         continue
+#     else:
+#         inu1.append(i)
+# print(inu1)
+
+
