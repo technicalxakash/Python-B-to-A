09c51ad42b57ce49826a5fcfd3c4e90ac5e25311
technicalxakash <akashr2468@gmail.com>
2026-02-04 13:09:10 +0530

    new

---
+++ Day10/10.py
@@ -0,0 +1,16 @@
+
+
+num=list(map(int,input("Enter the List : ").split(",")))
+
+
+num1=int(input("enter the number : "))
+count=0
+for i in num:
+    if i==num1:
+        count=count+1
+print(count)
+
+
+
+
+
