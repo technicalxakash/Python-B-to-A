14a96e76d6be546ff9c3cf9d4544c9d802529ce0
technicalxakash <akashr2468@gmail.com>
2026-02-06 22:04:50 +0530

    new

---
+++ Day11/11.py
@@ -0,0 +1,36 @@
+# num=2035
+# if(num%4==0):
+#     if num%400==0 or num%100!=0:
+#         print("it is leap year")
+#     else:
+#         print("it is not leap year")
+# else:
+#     print(print("it is not leap year"))
+
+
+#Loops
+#Print 1 to N
+# n=int(input("enter the number"))
+# for i in range(1,n+1):
+#     print(i,end=" ")
+
+
+#Sum of first N numbers
+# n=5
+# count=0
+
+# for i in range(1,6):
+#     count=count+i
+# print(count)
+
+#Factorial
+
+
+num=[5]
+factt=1
+for i in num:
+    factt=factt*i
+    i=i-1
+print(factt)
+
+
