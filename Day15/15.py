2b03a7d3088b9c4f33f8a0040cbb06a5ece20b91
technicalxakash <akashr2468@gmail.com>
2026-02-18 10:54:36 +0530

    new

--- Day15/15.py
+++ Day15/15.py
@@ -1,5 +1,31 @@
 #Largest number
-num=[1,2,3,4,5,99,6,65,89]
+# num=[1,2,3,4,5,99,6,65,89]
 
-for i in num:
-    
+# # num.sort()
+# # print(num[-1])
+
+# largest=num[0]
+
+# for i in num:
+#     if i>largest:
+#         largest=i
+# print(largest)
+
+
+#prime numbers
+# num=35
+# is_prime=True
+
+# for i in range(2,(num//2)+1):
+#     if(num%i==0):
+#         is_prime=False
+#         break
+
+# if is_prime==True:
+#     print("it is a prime number")
+# else:
+#     print("it is not a prime number")
+
+
+#count of digits
+
