70ac6dbd88359bd0fc1c6d5069ac301387cda114
technicalxakash <akashr2468@gmail.com>
2026-02-24 17:49:09 +0530

    new

--- Day17/17.py
+++ Day17/17.py
@@ -35,3 +35,52 @@
 #     sec=third
 #     num=num-1
 #     print(third)
+
+
+#Constructors
+# class Dog:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+    
+#     def show(self):
+#         print(f"{self.name} this dog age is {self.age}")
+
+# dog1=Dog("scuby",6)
+
+# dog1.show()
+
+
+#wihtout Duplicates
+
+# num=[1,2,3,4,5,6,78,6,2,3]
+
+# uni=[]
+
+# for i in num:
+#     if i not in uni:
+#         uni.append(i)
+# print(uni)
+
+# num=[1,2,3,4,5,6,78,6,2,3]
+
+# uni=list(set(num))
+# print(uni)
+
+
+#print duplicate eleemnrs
+
+# num=[1,2,3,4,5,6,78,6,2,3]
+
+# dup=[]
+# seen=[]
+
+# for i in num:
+#     if i in seen and i not in dup:
+#         dup.append(i)
+#     else:
+#         seen.append(i)
+
+# print(dup)
+
+
