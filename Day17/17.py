dd807de6cdce139661f598a13c799de7037710d5
technicalxakash <akashr2468@gmail.com>
2026-02-24 13:00:37 +0530

    new

---
+++ Day17/17.py
@@ -0,0 +1,37 @@
+# #Palindrome number
+# num="racecar"
+
+# # print(num[::-1])
+
+# # new=""
+
+# # for i in str(num):
+# #     print(i,end="")
+# numm=""
+
+# for ch in num:
+#     numm=ch+numm
+# #print(numm,end="")
+
+
+
+# if(num==numm):
+#     print("it is a palindrome")
+# else:
+#     print("it is a not palindrome")
+
+
+#Fibonacci N terms
+
+# num=5
+# first=0
+# sec=1
+
+# print(first)
+# print(sec)
+# while num>=0:
+#     third=first+sec
+#     first=sec
+#     sec=third
+#     num=num-1
+#     print(third)
