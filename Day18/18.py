768bba17f7df872e6e7ba40f5095e1c65b03ffe0
technicalxakash <akashr2468@gmail.com>
2026-02-28 23:41:57 +0530

    new

--- Day18/18.py
+++ Day18/18.py
@@ -1,11 +1,34 @@
-a=[1,2,3,4,5,6]
-b=[7,8,9]
-c=[]
+# a=[1,2,3,4,5,6]
+# b=[7,8,9]
+# c=[]
 
-for i in a:
-    c.append(i)
+# for i in a:
+#     c.append(i)
 
-for i in b:
-    c.append(i)
+# for i in b:
+#     c.append(i)
 
-print(c)
+# print(c)
+
+
+#Print primes till N
+
+
+#reversing a number
+#method 1
+# a=12345
+# aa=""
+# for i in str(a):
+#     aa=i+aa
+# print(int(aa))
+
+#method 2
+
+# a=12345
+# last=0
+# while a>0:
+#     digi=a%10
+#     last=last*10+digi
+#     a=a//10
+# print(last)
+
