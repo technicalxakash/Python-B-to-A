4dcb0c17d5528846cc9109378b44886a574013a9
technicalxakash <akashr2468@gmail.com>
2026-03-26 12:46:57 +0530

    new

--- Day22/22.py
+++ Day22/22.py
@@ -1,18 +1,51 @@
 
 #count of vowels and consonents
-strr="aakash  AAwAkash"
-str=strr.lower()
-count=0
-count1=0
-for ch in str:
-    if ch==" ":
-        continue
-    elif ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u":
-        count=count+1
-    else:
-        count1=count1+1
+# strr="aakash  AAwAkash"
+# str=strr.lower()
+# count=0
+# count1=0
+# for ch in str:
+#     if ch==" ":
+#         continue
+#     elif ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u":
+#         count=count+1
+#     else:
+#         count1=count1+1
 
-print(count)
-print(count1)
+# print(count)
+# print(count1)
 
 #to find total numebr of Capital and smaller cases vowels
+
+# str="AEIOUqqqqaeiouuu"
+# count=0
+# count1=0
+
+# for ch in str:
+#     if ch==" ":
+#          continue
+#     elif ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u":
+#          count=count+1
+#     elif ch=="A" or ch=="E" or ch=="I" or ch=="O" or ch=="U":
+#         count1=count1+1
+
+# print(count)
+# print(count1)
+
+
+#write a program to convert all uppercase to lowercase, lowercase to upper
+
+strr="aaaaaaaAAAAAA"
+
+#print(strr.swapcase())
+res=""
+
+
+for ch in strr:
+    if ch>='a' and ch<='z':
+        res=res+chr(ord(ch)-32)
+    elif ch>='A' and ch<='Z':
+        res=res+chr(ord(ch)+32)
+    else:
+        res=res+ch
+print(res)
