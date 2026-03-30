d5f1a9e5ac76e3ae1e338cc892ff20c1d2aa33fb
technicalxakash <akashr2468@gmail.com>
2026-03-30 08:55:38 +0530

    new

--- DAy24/24.py
+++ DAy24/24.py
@@ -3,5 +3,19 @@
 strr="hi god how are you"
 d=""
 for i in strr.split():
-    d=d+strr[::-1]+" "
-    print(d)
+    d=d+i[::-1]+" "
+print(d)
+
+
+#find the sum of all ovules in the String
+a="aeiouAEIOU"
+buc=0
+for i in a:
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u" or i=="A" or i=="E" or i=="I" or i=="O" or i=="U":
+        buc=buc+ord(i)
+print(buc)    
+
+a="qadehhio"
+
+for i in a:
+    if 
